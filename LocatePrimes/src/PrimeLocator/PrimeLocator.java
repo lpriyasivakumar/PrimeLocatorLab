f0cd73dd337c3e35613f231392254fcf0185ffac
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2015-07-07 09:57:36 -0400

    Refactored to pull out checkPrimes as a method to check if number is a prime number

--- LocatePrimes/src/PrimeLocator/PrimeLocator.java
+++ LocatePrimes/src/PrimeLocator/PrimeLocator.java
@@ -3,8 +3,9 @@ package PrimeLocator;
 import java.util.ArrayList;
 
 public class PrimeLocator {
+	private static final int MAX_NUM = 1000;
 	public static ArrayList<Integer> primes = new ArrayList<Integer>();
-
+	
 	public static int locate(int index) {
 		primes = populatePrimes(index - 1);
 		return primes.get(index - 1);
@@ -12,28 +13,33 @@ public class PrimeLocator {
 
 	private static ArrayList<Integer> populatePrimes(int index) {
 		while (primes.size() <= index) {
-			for (int num = 2; num < 1000; num++) {
-				if (num < 4)
-					primes.add(num);
-				else if (num % 2 == 0 || num % 3 == 0)
-					continue;
-				else if (trialDivision(num))
-					primes.add(num);
-			}
+			checkIfPrime();
 		}
 		return primes;
 	}
 
-	private static boolean trialDivision(int num) {
-		int sqrt = (int) Math.sqrt(num);
-		boolean isPrime = true;
-		for (int i = 2; i <= sqrt; i++) {
-			if (num % i == 0) {
-				isPrime = false;
-				break;
+	private static void checkIfPrime() {
+		for (int num = 2; num < MAX_NUM; num++) {
+			if (num < 4){
+				primes.add(num);
+			}
+			else if (num % 2 == 0 || num % 3 == 0){
+				continue;
+			}
+			else if (trialDivision(num)){
+				primes.add(num);
 			}
 		}
-		return isPrime;
+	}
+
+	private static boolean trialDivision(int num) {
+		int sqrt = (int) Math.sqrt(num);		
+		for (int i = 2; i <= sqrt; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
 
 	}
 
